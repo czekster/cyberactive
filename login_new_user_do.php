a9d7b84fc9795a944e122e9bba778ebf7104ec5d
Roberto Metere <roberto@metere.it>
2021-11-19 09:20:08 +0000

    fix creation of new users

--- login_new_user_do.php
+++ login_new_user_do.php
@@ -3,8 +3,8 @@ session_start();
 
 include_once('globals.php');
 $email = $_POST['email'];
-$passwd = $_POST['passwd'];
-$passwd2 = $_POST['passwd2'];
+$passwd = hash('sha512', $_POST['passwd']);
+$passwd2 = hash('sha512', $_POST['passwd2']);
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL) || $passwd == "" || $passwd2 == "" || $passwd != $passwd2) {
    $_SESSION['message'] = "Invalid email or empty or invalid password.";
